086678e2b258b42fb201b999604b4b7fca021cb1
Michael Howard <mike@hoorayfor3d.com>
2015-12-30 12:14:36 -0500

    Requested changes
    Change the about text, studio links, and added phone link. Working on
    adding custom pages as well.

--- js/creative.js
+++ js/creative.js
@@ -42,6 +42,12 @@
         }
     })
 
+    // Select all elements with data-toggle="tooltips" in the document
+    $('[data-toggle="tooltip"]').tooltip(); 
+
+    // Select a specified element
+    $('#myTooltip').tooltip();
+
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
 
